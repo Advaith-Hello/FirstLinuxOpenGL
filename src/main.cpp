a1bd450e658c7552fbaec34f8b2717e475756b2b
Advaith_Hello <advaith.anoopr@gmail.com>
2026-07-04 17:26:37 -0500

    Black Window

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,55 @@
 #include <iostream>
 
 
-int main(void) {
-  std::cout << "Hello World!" << std::endl;
+
+constexpr int WINDOW_X = 800;
+constexpr int WINDOW_Y = 600;
+
+
+// Resizing window
+void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
+  glViewport(0, 0, width, height);
+}
+
+
+int main(void)
+{
+  // GLFW Init
+  glfwInit();
+
+  // OpenGL 3.3 (Core)
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+  glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+
+  // Window creation
+  GLFWwindow* window = glfwCreateWindow(WINDOW_X, WINDOW_Y, "Triangle", NULL, NULL);
+  if (window == NULL) {
+    std::cout << "Failed to create window (GLFW)" << std::endl;
+    glfwTerminate();
+    return -1;
+  }
+  glfwMakeContextCurrent(window);
+
+  // Glad init
+  if (!gladLoadGL(glfwGetProcAddress)) {
+    std::cout << "Failed to initialize GLAD" << std::endl;
+    return -2;
+  }
+
+  // OpenGL init
+  glViewport(0, 0, WINDOW_X, WINDOW_Y);
+  glfwSetFramebufferSizeCallback(window, framebuffer_size_callback); 
+  
+  // Mainloop
+  while(!glfwWindowShouldClose(window)) {
+    glfwSwapBuffers(window);
+    glfwPollEvents();
+  }
+
+  // Cleanup
+  glfwTerminate();
+  return 0;
 }
 
